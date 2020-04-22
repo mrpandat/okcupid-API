1ec2d8c95c37f02b6b3576913db69bb8d71579d8
Juan <mrpandat@hotmail.fr>
2020-04-22 16:10:38 +0200

    Try to login through graphql.

--- custom_modules/constants.js
+++ custom_modules/constants.js
@@ -1,23 +1,28 @@
 module.exports = {
+    mutations: {
+      logout: 'mobileWebLogoutUser',
+      login: 'authUserLoginWithEmail'
+    },
     URLS: {
-    login: 'https://www.okcupid.com/login',
-    rate: 'http://www.okcupid.com/quickmatch',
-    visit_user: 'http://www.okcupid.com/profile/{username}',
-    user_profile: 'http://www.okcupid.com/profile/{username}?okc_api=1',
-    get_visitors: 'http://www.okcupid.com/visitors?okc_api=1',
-    quickmatch: 'http://www.okcupid.com/quickmatch?okc_api=1',
+      graphql: 'https://www.okcupid.com/graphql',
+      login: 'https://www.okcupid.com/login',
+      rate: 'http://www.okcupid.com/quickmatch',
+      visit_user: 'http://www.okcupid.com/profile/{username}',
+      user_profile: 'http://www.okcupid.com/profile/{username}?okc_api=1',
+      get_visitors: 'http://www.okcupid.com/visitors?okc_api=1',
+      quickmatch: 'http://www.okcupid.com/quickmatch?okc_api=1',
 
-    // OAuth API
-    get_messages: 'https://www.okcupid.com/1/apitun/messages',
-    get_thread: 'https://www.okcupid.com/1/apitun/messages/conversations/{thread_id}',
+      // OAuth API
+      get_messages: 'https://www.okcupid.com/1/apitun/messages',
+      get_thread: 'https://www.okcupid.com/1/apitun/messages/conversations/{thread_id}',
 
-    like: 'https://www.okcupid.com/1/apitun/profile/{userid}/like',
-    unlike: 'https://www.okcupid.com/1/apitun/profile/{userid}/unlike',
-    send_message: 'https://www.okcupid.com/1/apitun/messages/send',
-    search: 'https://www.okcupid.com/1/apitun/match/search',
-    edit_profile: 'https://www.okcupid.com/1/apitun/profile/save_essay',
-    connections: 'https://www.okcupid.com/1/apitun/connections/outgoing',
-    user_questions: 'http://www.okcupid.com/1/apitun/profile/{username}/answers',
+      like: 'https://www.okcupid.com/1/apitun/profile/{userid}/like',
+      unlike: 'https://www.okcupid.com/1/apitun/profile/{userid}/unlike',
+      send_message: 'https://www.okcupid.com/1/apitun/messages/send',
+      search: 'https://www.okcupid.com/1/apitun/match/search',
+      edit_profile: 'https://www.okcupid.com/1/apitun/profile/save_essay',
+      connections: 'https://www.okcupid.com/1/apitun/connections/outgoing',
+      user_questions: 'http://www.okcupid.com/1/apitun/profile/{username}/answers',
   },
 }
 
